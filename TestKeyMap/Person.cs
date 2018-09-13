5d565492e52e67d08da044533e4aca17caf54bd5
Joni <j.atisto@gmail.com>
2018-09-13 16:23:58 +0600

    Дописал код

---
+++ TestKeyMap/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestKeyMap
+{
+    internal partial class Program
+    {
+        internal partial class Person
+        {
+            public string N { get; set; }
+            public int A { get; set; }
+
+            public Person(string n, int a)
+            {
+                N = n;
+                A = a;
+            }
+
+
+            public override string ToString()
+            {
+                return $"{N} - {A}";
+            }
+        }
+    }
+}
---
+++ TestKeyMap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestKeyMap
+{
+    internal partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Nik", 15);
+            Console.WriteLine($"{person.ToString()}");
+            Console.WriteLine($"Hell Hllo {person.ToString()}");
+            Console.ReadKey();
+        }
+    }
+}
